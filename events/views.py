a761c372a4ceadc12b7556131dca45e4cb112abd
orlagh-sweeney <orlagh16@gmail.com>
2023-09-04 19:12:55 +0000

    Add EventDetail view

--- events/views.py
+++ events/views.py
@@ -1,8 +1,10 @@
+from django.http import Http404
 from rest_framework import status, permissions
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from .models import Event
 from .serializers import EventSerializer
+from drf_event_api.permissions import IsOwnerOrReadOnly
 
 
 class EventList(APIView):
@@ -29,4 +31,43 @@ class EventList(APIView):
             )
         return Response(
             serializer.errors, status=status.HTTP_400_BAD_REQUEST
+        )
+
+
+class EventDetail(APIView):
+    permission_classes = [IsOwnerOrReadOnly]
+    serializer_class = EventSerializer
+
+    def get_object(self, pk):
+        try:
+            event = Event.objects.get(pk=pk)
+            self.check_object_permissions(self.request, event)
+            return event
+        except Event.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk):
+        event = self.get_object(pk)
+        serializer = EventSerializer(
+            event, context={'request': request}
+        )
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        event = self.get_object(pk)
+        serializer = EventSerializer(
+            event, data=request.data, context={'request': request}
+        )
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(
+            serializer.errors, status=status.HTTP_400_BAD_REQUEST
+        )
+
+    def delete(self, request, pk):
+        event = self.get_object(pk)
+        event.delete()
+        return Response(
+            status=status.HTTP_204_NO_CONTENT
         )
