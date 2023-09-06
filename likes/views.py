8599568e734df28af0ea88a0668361adf18b5d36
orlagh-sweeney <orlagh16@gmail.com>
2023-09-06 15:11:38 +0000

    Add like app serializers and views

--- likes/views.py
+++ likes/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from drf_event_api.permissions import IsOwnerOrReadOnly
+from .models import Like
+from .serializers import LikeSerializer
 
-# Create your views here.
+
+class LikeList(generics.ListCreateAPIView):
+    """
+    List comments or create a like if logged in.
+    """
+    serializer_class = LikeSerializer
+    # only authenticated users can like comments
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    queryset = Like.objects.all()
+
+    # associate likes with a user upon creation
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class LikeDetail(generics.RetrieveDestroyAPIView):
+    """
+    Retrieve a like or delete it by id if you own it.
+    """
+    # only the user who liked a comment can unlike it
+    permission_classes = [IsOwnerOrReadOnly]
+    serializer_class = LikeSerializer
+    queryset = Like.objects.all()
