9cb4832f3235445ee524207519a761244420985f
orlagh-sweeney <orlagh16@gmail.com>
2023-09-06 14:53:14 +0000

    Create likes app and model

--- drf_event_api/settings.py
+++ drf_event_api/settings.py
@@ -54,6 +54,7 @@ INSTALLED_APPS = [
     'profiles',
     'events',
     'comments',
+    'likes',
 ]
 
 MIDDLEWARE = [
---
+++ likes/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+from django.contrib.auth.models import User
+from comments.models import Comment
+
+
+class Like(models.Model):
+    """
+    Like model, related to 'owner' and 'comment'.
+    'owner' is a User instance and 'comment' is a Comment instance.
+    """
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    comment = models.ForeignKey(Comment, related_name='likes', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-created_at']
+        # a user cannot like the same comment twice
+        unique_together = ['owner', 'comment']
+
+    def __str__(self):
+        return f"{self.comment} {self.owner}"
