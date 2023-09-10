4e1a7ce1818f837ef22964ade70c8cefba7d174b
orlagh-sweeney <orlagh16@gmail.com>
2023-09-10 20:31:30 +0000

    Add user interests to profile serializer

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import Profile
+from interests.models import Interest
 
 
 class ProfileSerializer(serializers.ModelSerializer):
@@ -8,15 +9,28 @@ class ProfileSerializer(serializers.ModelSerializer):
     events_count = serializers.ReadOnlyField()
     attended_count = serializers.ReadOnlyField()
     attending_count = serializers.ReadOnlyField()
+    user_interests = serializers.SerializerMethodField()
 
     def get_is_owner(self, obj):
         request = self.context['request']
         return request.user == obj.owner
+
+    # get all interests belonging to a profile
+    def get_user_interests(self, obj):
+        interests = Interest.objects.all()
+        user_interests = {}
+        for i in interests:
+            owner = str(i.owner)
+            if owner not in user_interests.keys():
+                user_interests[owner] = [i.type]
+            else:
+                user_interests[owner] = user_interests[owner] + [i.type]
+        return user_interests[str(obj.owner)]
 
     class Meta:
         model = Profile
         fields = [
             'id', 'owner', 'created_at', 'updated_at', 'name',
             'content', 'image', 'is_owner', 'events_count', 'attended_count',
-            'attending_count',
+            'attending_count', 'user_interests',
         ]
